78049f1d26cde81c653016a413280b9179171668
Shireesha Basani <165460672+shireeshabasani@users.noreply.github.com>
2025-04-21 21:09:10 +0530

    Update trying.java

--- trying.java
+++ trying.java
@@ -2,5 +2,5 @@ import java.util.*;
 
 public class trying{
      public static void main(String args[]){
-     printf("Iam in horrible situation ...");
+     System.out.println("Iam in horrible situation ...");
      }}
